17ef8daa34fa95faf7968352d50768cbd763e530
Marcin Noga <marcin@careaboutit.pl>
2023-02-06 17:06:44 +0100

    remove public modifier in ResultCheckerMapper

--- src/main/java/pl/lotto/resultchecker/ResultCheckerMapper.java
+++ src/main/java/pl/lotto/resultchecker/ResultCheckerMapper.java
@@ -3,7 +3,7 @@ package pl.lotto.resultchecker;
 import pl.lotto.numberreceiver.dto.TicketDto;
 import pl.lotto.resultchecker.dto.PlayerResultDto;
 
-public class ResultCheckerMapper {
+class ResultCheckerMapper {
 
     public static TicketEntity mapTicketDtoToTicket(TicketDto ticketDto){
         return new TicketEntity(ticketDto.lotteryId(),ticketDto.numbers(),ticketDto.drawDate());
