8eca04bf273706e903871833b306814fc8a13749
Marcin Noga <marcin@careaboutit.pl>
2023-04-14 13:32:39 +0200

    added cors mappings

--- src/main/java/pl/lotto/LottoApplication.java
+++ src/main/java/pl/lotto/LottoApplication.java
@@ -2,10 +2,7 @@ package pl.lotto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,18 +11,5 @@ public class LottoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LottoApplication.class, args);
     }
-
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedMethods("*")
-//                        .allowedOrigins("http://localhost:3000");
-//            }
-//        };
-//    }
 
 }
---
+++ src/main/java/pl/lotto/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package pl.lotto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
+}
